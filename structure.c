a453f388df2606369795b64de61b104711810b20
victorlin7534 <vlin5@stuy.edu>
2018-10-03 11:43:22 -0400

    2

--- structure.c
+++ structure.c
@@ -7,8 +7,13 @@
 int main(){
   struct one test = example();
   show(test);
-  struct one * temp = &test;
-  modify(temp,23.2,"cake");
+  modify(&test,23.2,"cake");
+  show(test);
+  modify(&test,1.23,"potato");
+  show(test);
+  modify(&test,0.65,"egg");
+  show(test);
+  modify(&test,100000.02,"bean");
   show(test);
   return 0;
 }
@@ -17,16 +22,17 @@ struct one example(){
   struct one e;
   srand(time(NULL));
   e.string = "banana";
-  e.val = rand();
+  e.val = rand()%100;
   return e;
 }
 
 void show(struct one x){
-  printf("struct val: %lf\n", x.val);
-  printf("struct string: %s\n",x.string);
+  printf("food name: %s\n",x.string);
+  printf("food price: $%.2lf\n", x.val);
 }
 
 void modify(struct one *  x, double mv, char * ns){
+  printf("\nchanging food item...\n\n");
   x->val = mv;
   x->string = ns;
 }
